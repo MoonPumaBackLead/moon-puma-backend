a5cd742b688e175f4d4ae16117f4b01a872ba710
Vlad <Smirnov.ru92@mail.ru>
2026-05-21 21:38:05 +0500

    prepare for deploy api application

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@libs/prisma';
+import { PrismaClient } from '@/libs/prisma';
 
 @Injectable()
 export class PrismaService extends PrismaClient {}
